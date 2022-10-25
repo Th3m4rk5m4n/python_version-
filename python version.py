e7f6336718363515d4c1f45913ffb03d1c8d2aa0
5kipp3r <112339635+jayvin1244@users.noreply.github.com>
2022-10-25 18:24:28 +0530

    Add files via upload

---
+++ python version.py
@@ -0,0 +1,14 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+s= RESTART: C:/Users/jayvingohel/AppData/Local/Programs/Python/Python310/format function.py
+hello mohan your age is 22
+
+= RESTART: C:/Users/jayvingohel/AppData/Local/Programs/Python/Python310/format function.py
+hello mohan your age is 22
+hello mohan youe age is 22
+
+= RESTART: C:/Users/jayvingohel/AppData/Local/Programs/Python/Python310/format function.py
+hello mohan your age is 22
+hello mohan youe age is 22
+hello mohan your age is 22
